c4265ae31dc2d7091ca14d224d0f87c667050029
Concellia <tiyiselani.rhangani@gmail.com>
2020-08-02 01:25:12 +0200

    app

--- runtime-terror/src/app/patient-landing-page/patient-landing-page.page.ts
+++ runtime-terror/src/app/patient-landing-page/patient-landing-page.page.ts
@@ -6,103 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./patient-landing-page.page.scss'],
 })
 export class PatientLandingPagePage implements OnInit {
-  doctors = [
-    {
-      name: "Dr Mokoena",
-      practice: "General Practitioner",
-      address: "250 Marshall street Johannesburg",
-      contact: "0115268745",
-      time: "08:00-16:00",
-      rating: "5",
-      distance: "50km Away"
-    },
-    {
-      name: "Dr Laubuschagne",
-      practice: "Dentist",
-      address: "250 Marshall street Johannesburg",
-      contact: "0115268562",
-      time: "10:00-16:00",
-      rating: "5",
-      distance: "900km Away"
-    },
-    {
-      name: "Dr Smith",
-      practice: "General Practitioner",
-      address: "250 Marshall street Johannesburg",
-      contact: "0115268895",
-      time: "08:00-17:00",
-      rating: "4",
-      distance: "23km Away"
-    },
-    {
-      name: "Dr Cho",
-      practice: "Pediatricians",
-      address: "250 Marshall street Johannesburg",
-      contact: "0115268856",
-      time: "08:00-14:00",
-      rating: "3",
-      distance: "49km Away"
-    },
-    {
-      name: "Dr Sthole",
-      practice: "Allergist",
-      address: "250 Marshall street Johannesburg",
-      contact: "0115268781",
-      time: "07:00-15:00",
-      rating: "4",
-      distance: "1km Away"
-    },
-    {
-      name: "Dr Chavalala",
-      practice: "Dermatologist",
-      address: "250 Marshall street Johannesburg",
-      contact: "01152687584",
-      time: "08:00-18:00",
-      rating: "5",
-      distance: "200km Away"
-    },
-    {
-      name: "Dr Doe",
-      practice: "Dentist",
-      address: "250 Marshall street Johannesburg",
-      contact: "0115268798",
-      time: "08:00-16:00",
-      rating: "2",
-      distance: "15km Away"
-    },
-    {
-      name: "Dr George",
-      practice: "Neurologists",
-      address: "250 Marshall street Johannesburg",
-      contact: "0115268752",
-      time: "08:00-16:00",
-      rating: "5",
-      distance: "52km Away"
-    },
-    {
-      name: "Dr Ramabulana",
-      practice: "General Practitioner",
-      address: "250 Marshall street Johannesburg",
-      contact: "0115268523",
-      time: "08:00-16:00",
-      rating: "5",
-      distance: "200km Away"
-    },
-    {
-      name: "Dr Khosa",
-      practice: "General Practitioner",
-      address: "250 Marshall street Johannesburg",
-      contact: "011526875642",
-      time: "08:00-16:00",
-      rating: "2",
-      distance: "50km Away"
-    }
+  doctors: any;
+  rate = [];
+rating()
+{
 
-  ]
-
+}
   constructor() { }
 
   ngOnInit() {
+    fetch('./assets/data/doctor.json').then(res => res.json())
+    .then(json => {
+      this.doctors = json;
+      for(let i = 0; i<json.length; i++)
+      {
+        if(json[i].rating == "5")
+        {
+          this.rate.push(json[i])
+        }
+      }
+    });
   }
 
 }
